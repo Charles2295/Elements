18fccf5ce324b1901f996e09238b2ab48a22b353
Charles2295 <lawrenson2295@gmail.com>
2021-04-07 22:44:13 +0100

    - major steps forward for InfoBox, using variable like method to change values and colours etc

--- TreeSize/MainWindow.xaml.cs
+++ TreeSize/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -28,34 +30,48 @@ namespace TreeSize
             DisablingInfoBox();
         }
 
-        private void DisablingInfoBox()
+        private void DisablingInfoBox() // This is the default state
         {
-            InfoBox1.Visibility = Visibility.Hidden;
+            InfoBox.Visibility = Visibility.Hidden;
             InfoBoxTitle.Visibility = Visibility.Hidden;
             AtomicNumberTitle.Visibility = Visibility.Hidden;
             AtomicNumberValue.Visibility = Visibility.Hidden;
             AtomicWeightTitle.Visibility = Visibility.Hidden;
             AtomicWeightValue.Visibility = Visibility.Hidden;
+            AtomicElectroTitle.Visibility = Visibility.Hidden;
+            AtomicElectroValue.Visibility = Visibility.Hidden;
         }
 
         private void EnablingInfoBox()
         {
-            InfoBox1.Visibility = Visibility.Visible;
+            InfoBox.Visibility = Visibility.Visible;
             InfoBoxTitle.Visibility = Visibility.Visible;
             AtomicNumberTitle.Visibility = Visibility.Visible;
             AtomicNumberValue.Visibility = Visibility.Visible;
             AtomicWeightTitle.Visibility = Visibility.Visible;
             AtomicWeightValue.Visibility = Visibility.Visible;
+            AtomicElectroTitle.Visibility = Visibility.Visible;
+            AtomicElectroValue.Visibility = Visibility.Visible;
         }
 
         private void HydrogenButton_Click(object sender, RoutedEventArgs e)
         {
             EnablingInfoBox();
+            InfoBoxTitle.Text = "Hydrogen";
+            InfoBoxTitle.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF80CBC4"));
+            AtomicNumberValue.Text = "1234";
+            AtomicWeightValue.Text = "1234";
+            AtomicElectroValue.Text = "1234";
         }
 
-        private void AtomicWeightTitle_TextChanged(object sender, TextChangedEventArgs e)
+        private void LithiumButton_Click(object sender, RoutedEventArgs e)
         {
-
+            EnablingInfoBox();
+            InfoBoxTitle.Text = "Lithium";
+            InfoBoxTitle.Foreground = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFAB91"));
+            AtomicNumberValue.Text = "123";
+            AtomicWeightValue.Text = "123";
+            AtomicElectroValue.Text = "123";
         }
     }
 }
